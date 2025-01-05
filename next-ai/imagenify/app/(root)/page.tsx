0a7cdf88259ecd7232274ab20b06f8039d85f7ec
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2025-01-05 16:27:00 +0530

    Feat: add the Nav links for desktop and mobile

--- next-ai/imagenify/app/(root)/page.tsx
+++ next-ai/imagenify/app/(root)/page.tsx
@@ -1,11 +1,12 @@
+"use client"
+
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <p>Home</p>
-      <UserButton/>
+      Home
     </div>
   )
 }
